726b1d2aac8e17255364ec56cd1be5dfd01a8ad3
Santiago Palladino <spalladino@gmail.com>
2020-05-12 20:19:47 -0300

    Change stack name

--- scripts/collect-stack-output.sh
+++ scripts/collect-stack-output.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+## TODO: Get from samconfig.toml
 # Stack name
-STACKNAME=${STACKNAME:-"sam-rest-test"}
+STACKNAME=${STACKNAME:-"sam-rest-dev-2"}
 
 # Collect stack info and reformat it
 aws cloudformation describe-stacks --stack-name $STACKNAME --query 'Stacks[0].Outputs' | \
