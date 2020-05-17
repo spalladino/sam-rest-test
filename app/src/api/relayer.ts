5229ae2d4c7b27dc45d3b202615d6fce85e13259
Steven Landers <steven.landers@gmail.com>
2020-05-17 19:31:29 -0400

    prettier

--- app/src/api/relayer.ts
+++ app/src/api/relayer.ts
@@ -1,6 +1,6 @@
-import { API } from 'aws-amplify';
+import { API } from "aws-amplify";
 
-const API_NAME = 'RelayerApi';
+const API_NAME = "RelayerApi";
 
 // quick and dirty random id generation
 function getRandomId(): string {
@@ -12,18 +12,18 @@ export interface Relayer {
   relayerId: string;
 }
 
-export async function createRelayer(name: string) : Promise<string> { 
-  const path = 'relayers';
+export async function createRelayer(name: string): Promise<string> {
+  const path = "relayers";
   const relayerId = getRandomId();
-  const relayer =  {
+  const relayer = {
     relayerId,
     name,
-  }
+  };
   await API.post(API_NAME, path, { body: relayer });
   return relayerId;
 }
 
-export async function getRelayers() : Promise<Relayer[]> {
+export async function getRelayers(): Promise<Relayer[]> {
   const path = `relayers`;
   const res = await API.get(API_NAME, path, {});
   return res as Relayer[];
