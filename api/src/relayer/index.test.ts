4044e048da368e87d82099d97c11beafa6a9d6f2
Steven Landers <steven.landers@gmail.com>
2020-05-20 08:58:14 -0400

    prettier

--- api/src/relayer/index.test.ts
+++ api/src/relayer/index.test.ts
@@ -5,18 +5,21 @@ jest.mock("../shared/db");
 
 describe("list", function () {
   test("list returns mock data", async function () {
-    const result = await list({
-      requestContext: {
-        authorizer: {
-          "tenantId": "test",
-          "dynamoCredentials": JSON.stringify({
-            "AccessKeyId": "access-key-id",
-            "SecretKey": "secret-key",
-            "SessionToken": "session-token",
-          })
-        }
-      }
-    } as unknown as APIGatewayProxyEvent, {} as Context);
+    const result = await list(
+      ({
+        requestContext: {
+          authorizer: {
+            tenantId: "test",
+            dynamoCredentials: JSON.stringify({
+              AccessKeyId: "access-key-id",
+              SecretKey: "secret-key",
+              SessionToken: "session-token",
+            }),
+          },
+        },
+      } as unknown) as APIGatewayProxyEvent,
+      {} as Context
+    );
     expect(result.statusCode).toEqual(200);
     expect(JSON.parse(result.body)).toEqual([{ relayerId: 1, name: "foo" }]);
   });
